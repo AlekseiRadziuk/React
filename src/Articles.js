40999a2b3528d3a54ff459a52698587700867eda
Aleksei Radziuk <Aleksei8893@gmail.com>
2018-06-06 22:32:52 +0300

    remove redudant files

--- src/Articles.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Article from './Article'
-
-export default function ArticleList({ articles }) {
-    return (<ul>
-        {articles.map((article) => <li key={article.id}><Article article={article} /></li>)}
-    </ul>)
-}
--- src/Comment.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export  default  function Comment({comment}) {
-	return (<div>{comment.user}: {comment.text}</div>);
-}
